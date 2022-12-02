4d3288674317d66054c4ae195719337c8d262c3f
MichaelKyrollos <michaelkyrollos@cmail.carleton.ca>
2022-12-01 22:25:34 -0500

    CLI

--- Bookstore/src/Bookstore.java
+++ Bookstore/src/Bookstore.java
@@ -1,2 +1,46 @@
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 public class Bookstore {
+    public void printWelcome()
+    {
+        Scanner input = new Scanner(System.in);
+        System.out.println("==================\n" +
+                "ONLINE BOOKSTORE\n" +
+                "==================" );
+        System.out.println("1/ Login \n" + "2/ Register \n" + "3/ Login as Manager");
+        do {
+            try {
+                switch ((input.nextInt())) {
+                    case 0:
+                        System.exit(0);
+                    case 1:
+                        User user = new User(this);
+                        user.userLogin();
+                        break;
+                    case 2:
+                        newUser();
+                        break;
+
+                    case 3:
+                        Manager manager = new Manager(this);
+                        manager.managerLogin();
+                        break;
+                }
+            }
+            catch(InputMismatchException e) {
+                System.out.println("Please enter a valid input");
+                printWelcome();
+            }
+        } while (!input.hasNextInt());
+    }
+
+    private void newUser() {
+    }
+
+    public static void main(String[] args) {
+        Bookstore bookstore = new Bookstore();
+        bookstore.printWelcome();
+    }
+
 }
--- Bookstore/src/Manager.java
+++ Bookstore/src/Manager.java
@@ -1,2 +1,67 @@
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 public class Manager {
+    Bookstore bookstore;
+
+    public Manager(Bookstore bookstore) {
+        this.bookstore = bookstore;
+    }
+
+    public void managerLogin() {
+        System.out.println("------------------\n" +
+                "  MANAGER LOGIN \n" +
+                "------------------\n" );
+        System.out.println("1/ Add book to database\n" +  "2/ Delete book\n" + "3/ Generate report\n" +
+                "4/ Add publisher\n" + "5/ Logout\n");
+        Scanner input = new Scanner(System.in);
+        do {
+            try {
+                switch ((input.nextInt())) {
+                    case 0:
+                    case 5:
+                        bookstore.printWelcome();
+                    case 1:
+                        managerAddBook();
+                        break;
+
+                    case 2:
+                        deleteBook();
+                        break;
+
+                    case 3:
+                        generateReport();
+                        break;
+
+                    case 4:
+                        addPublisher();
+                        break;
+
+                }
+            }
+            catch(InputMismatchException e) {
+                System.out.println("Please enter a valid input");
+                managerLogin() ;
+            }
+
+        } while (!input.hasNextInt());
+    }
+
+
+    public void addPublisher() {
+    }
+
+    public void generateReport() {
+    }
+
+    public void deleteBook() {
+        
+    }
+
+    public void managerAddBook() {
+    }
+
+
+
+
 }
