6de7682cdab3ab284ac7422ee2f9b633742e597f
pianoob <vickyynewyen@gmail.com>
2022-12-06 21:58:14 -0500

    updated all the methods in manager class

--- Bookstore/src/Manager.java
+++ Bookstore/src/Manager.java
@@ -7,42 +7,63 @@ public class Manager {
     Connection connection;
     Statement statement;
 
-
     public Manager(Bookstore bookstore) {
         this.bookstore = bookstore;
     }
 
     public void managerLogin() {
+        Scanner input = new Scanner(System.in);
+
+        try {
+            Class.forName("org.postgresql.Driver");
+            connection = DriverManager.getConnection("jdbc:postgresql://localhost:5432/OnlineBookstore", "postgres", "admin");
+            statement = connection.createStatement();
+            if (connection != null) {
+                System.out.println("Connection OK");
+                //return true;
+            } else {
+                System.out.println("Failed");
+                bookstore.printWelcome();
+                //return false;
+            }
+        } catch (Exception e) {
+            System.out.println(e);
+            bookstore.printWelcome();
+        }
+
+
         System.out.println("------------------\n" +
                 "  MANAGER LOGIN \n" +
                 "------------------\n" );
-        System.out.println("1/ Add book to database\n" +  "2/ Delete book\n" + "3/ Generate report\n" +
-                "4/ Add publisher\n" + "5/ Logout\n");
-        Scanner input = new Scanner(System.in);
+        System.out.println("1/ Add author\n" +  "2/ Add book\n" + "3/ Delete book\n" +
+                "4/ Add publisher\n" + "5/ Generate report\n" + "6/ Logout\n");
         do {
             try {
                 switch ((input.nextInt())) {
                     case 0:
                         bookstore.printWelcome();
 
-                    case 5:
-                        bookstore.printWelcome();
-
                     case 1:
-                        managerAddBook();
+                        addAuthor();
                         break;
 
                     case 2:
-                        deleteBook();
+                        addBook();
                         break;
 
                     case 3:
-                        generateReport();
+                        deleteBook();
                         break;
 
                     case 4:
                         addPublisher();
                         break;
+
+                    case 5:
+                        generateReport();
+
+                    case 6:
+                        bookstore.printWelcome();
 
                 }
             }
@@ -56,7 +77,6 @@ public class Manager {
 
 
     public void addPublisher() {
-
         Scanner input = new Scanner(System.in);
         String email = "";
         String name = "";
@@ -116,6 +136,19 @@ public class Manager {
                 "REMOVE BOOK \n" +
                 "------------------" );
 
+        try {
+            result = statement.executeQuery(
+                    "SELECT isbn, name FROM book;"
+            );
+            while(result.next())  {
+                System.out.print("ISBN: " + result.getString("isbn"));
+                System.out.println("\tName: " + result.getString("name"));
+            }
+            System.out.println();
+        } catch (SQLException sqle) {
+            System.out.println(sqle);
+        }
+
         while (isbn.isEmpty()) {
             System.out.println("Please enter the ISBN of the Book you would like to remove: ");
             isbn = input.nextLine();
@@ -126,7 +159,29 @@ public class Manager {
         deleteBookFromDB(isbn);
     }
 
-    public void managerAddBook() {
+    public void addAuthor() {
+        Scanner input = new Scanner(System.in);
+        String email = "";
+        String name = "";
+        System.out.println("\n------------------\n" +
+                "ADD AUTHOR \n" +
+                "------------------" );
+
+        while (email.isEmpty() || name.isEmpty()) {
+            System.out.print("Email: ");
+            email = input.nextLine();
+            System.out.print("Name: ");
+            name = input.nextLine();
+            if (email.isEmpty() || name.isEmpty())
+                System.out.println("Please enter a valid input.\n");
+        }
+
+        addAuthorToDB(email, name);
+
+        managerLogin();
+    }
+
+    public void addBook() {
         Scanner input = new Scanner(System.in);
         String isbn = "";
         String name = "";
@@ -134,6 +189,8 @@ public class Manager {
         String quantity = "";
         String price = "";
         String pages = "";
+        String genreNum = "";
+        String genre = "";
 
         System.out.println("\n------------------\n" +
                 "ADD BOOK \n" +
@@ -157,15 +214,77 @@ public class Manager {
                     || !isInteger(quantity) || !isFloat(price) || !isInteger(pages))
                 System.out.println("Please enter a valid input.\n");
         }
-        addBookToDB(isbn, name, publisher, quantity, price, pages);
+
+
+        if(!addBookToDB(isbn, name, publisher, quantity, price, pages)) {
+            managerLogin();
+        }
+
+        while (!isInteger(genreNum)) {
+            System.out.println("How many genres does this book have?");
+            genreNum = input.nextLine();
+            if(!isInteger(genreNum))
+                System.out.println("Please enter a valid number.");
+        }
+
+        for (int i = 0; i < Integer.parseInt(genreNum); i++) {
+            System.out.print("Genre #" + (i+1) + ": ");
+            genre = input.nextLine();
+            if(!addGenreToDB(isbn, genre)){
+                i--;
+                System.out.println("Please enter a valid input.");
+            }
+        }
+
+        addWrittenBy(isbn, name);
+        managerLogin();
+    }
+
+    public void addWrittenBy(String isbn, String name) {
+        Scanner input = new Scanner(System.in);
+        String authors = "";
+        int num = 1;
+        String email = "";
+
+        while (!isInteger(authors)) {
+            System.out.println("How many authors does this book have?");
+            authors = input.nextLine();
+            if(!isInteger(authors))
+                System.out.println("Please enter a valid number.");
+        }
+
+        System.out.println("Listing current authors in the database...");
+        try {
+            result = statement.executeQuery(
+                    "SELECT name, email FROM author;"
+            );
+            while(result.next())  {
+                System.out.println("Author #" + num++);
+                System.out.println("\tName: " + result.getString("name"));
+                System.out.println("\tEmail: " + result.getString("email"));
+            }
+            System.out.println();
+        } catch (SQLException sqle) {
+            System.out.println(sqle);
+        }
+
+        for (int i = 0; i < Integer.parseInt(authors); i++) {
+            System.out.println("Please enter the email of the author(s) of " + name + ":");
+            System.out.println("Note: If the author's name is not on this list, please enter 0 and add the author to the database");
+            email = input.nextLine();
+            if(Objects.equals(email, "0")) {
+                return;
+            }
+            if(!addWrittenByToDB(email, isbn)){
+                i--;
+                System.out.println("Please enter a valid input.");
+            }
+        }
+        return;
     }
 
     public boolean addPublisherToDB(String email, String name, String address, String bankingInfo) {
         System.out.println("Adding to database...");
-        if (!connect()) {
-            System.out.println("Error: Could not Connect to Database!");
-            return false;
-        }
 
         try {
             statement.executeUpdate("INSERT INTO publisher VALUES ('" + email + "', '" + name +
@@ -182,10 +301,6 @@ public class Manager {
 
     public boolean addPhoneNumToDB(String phone, String publisher) {
         System.out.println("Adding to database...");
-        if (!connect()) {
-            System.out.println("Error: Could not Connect to Database!");
-            return false;
-        }
 
         try {
             statement.executeUpdate("INSERT INTO PhoneNumbers VALUES ('" + publisher + "', '" + phone + "');");
@@ -198,12 +313,8 @@ public class Manager {
         return true;
     }
 
-    public void addBookToDB(String isbn, String name, String publisher, String quantity, String price, String pages) {
+    public boolean addBookToDB(String isbn, String name, String publisher, String quantity, String price, String pages) {
         System.out.println("Adding to database...");
-        if (!connect()) {
-            System.out.println("Error: Could not Connect to Database!");
-            return;
-        }
 
         try {
             statement.executeUpdate("INSERT INTO book VALUES ('" + isbn + "', '" + name + "', '" + publisher + "', '" + quantity + "', " + 0 + ", " +
@@ -212,16 +323,52 @@ public class Manager {
         } catch (SQLException sqle) {
             System.out.println("Error: Could not Add to Database!");
             System.out.println(sqle);
+            return false;
         }
-        managerLogin();
+
+        return true;
+
+    }
+
+    public boolean addGenreToDB(String isbn, String genre) {
+        System.out.println("Adding to database...");
+
+        try {
+            statement.executeUpdate("INSERT INTO genres VALUES ('" + isbn + "', '" + genre + "');");
+        } catch (SQLException sqle) {
+            System.out.println("Error: Could not Add to Database!");
+            System.out.println(sqle);
+            return false;
+        }
+        return true;
+    }
+
+    public void addAuthorToDB(String email, String name) {
+        System.out.println("Adding to database...");
+
+        try {
+            statement.executeUpdate("INSERT INTO author VALUES ('" + email + "', '" + name + "');");
+        } catch (SQLException sqle) {
+            System.out.println("Error: Could not Add to Database!");
+            System.out.println(sqle);
+        }
+    }
+
+    public boolean addWrittenByToDB(String email, String isbn) {
+        System.out.println("Adding to database...");
+
+        try {
+            statement.executeUpdate("INSERT INTO writtenBy VALUES ('" + email + "', '" + isbn + "');");
+        } catch (SQLException sqle) {
+            System.out.println("Error: Could not Add to Database!");
+            System.out.println(sqle);
+            return false;
+        }
+        return true;
     }
 
     public void deleteBookFromDB(String isbn) {
         System.out.println("Removing from database...");
-        if (!connect()) {
-            System.out.println("Error: Could not Connect to Database!");
-            return;
-        }
 
         try {
             statement.executeUpdate("DELETE FROM book WHERE (isbn='" + isbn + "');");
@@ -233,23 +380,6 @@ public class Manager {
         managerLogin();
     }
 
-    public boolean connect() {
-        try {
-            Class.forName("org.postgresql.Driver");
-            connection = DriverManager.getConnection("jdbc:postgresql://localhost:5432/OnlineBookstore", "postgres", "admin");
-            statement = connection.createStatement();
-            if (connection != null) {
-                System.out.println("Connection OK");
-                return true;
-            } else {
-                System.out.println("Failed");
-                return false;
-            }
-        } catch (Exception e) {
-            System.out.println(e);
-            return false;
-        }
-    }
 
     public static boolean isInteger(String s) {
         try {
